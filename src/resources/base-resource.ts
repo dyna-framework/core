566e379ea529c5d42ace06692be31e72c01e43c2
FreeIntelligence <free04381@gmail.com>
2022-02-28 02:04:27 -0300

    Se documentan las funciones.

--- src/resources/base-resource.ts
+++ src/resources/base-resource.ts
@@ -1,14 +1,29 @@
 import { Application } from "../application";
 
+/**
+ * Base resource class
+ */
 export class BaseResource {
 
+  /**
+   * Application instance
+   */
   public app: Application|null = null;
 
+  /**
+   * Set the application instance
+   * @param app application instance
+   * @returns resource instance
+   */
   setApplication(app: Application) {
     this.app = app;
     return this;
   }
 
+  /**
+   * Get the application instance
+   * @returns Application instance
+   */
   getApplication() {
     return this.app;
   }
--- src/resources/resources.ts
+++ src/resources/resources.ts
@@ -1,15 +1,29 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
+/**
+ * Resource
+ */
 export interface Resource {
   path: string;
   object: any;
 }
 
+/**
+ * Resources into application
+ */
 export class Resources {
 
+  /**
+   * Array container
+   */
   private container: Resource[] = [];
 
+  /**
+   * Load resources (files) from directory
+   * @param dir folder path
+   * @param recursive ĺoad files recursively
+   */
   loadFromDir(dir: string, recursive: boolean = false) {
     const files = fs.readdirSync(dir);
 
@@ -37,19 +51,38 @@ export class Resources {
     }
   }
 
+  /**
+   * Get all resources
+   * @returns resources loaded
+   */
   all() {
     return this.container;
   }
 
+  /**
+   * Get resources of type
+   * @param type type resource
+   * @returns resources loaded
+   */
   only(type: string) {
     return this.all().filter(e => e.object.INTERNAL_RESOURCE_TYPE === type);
   }
 
+  /**
+   * Push a resource
+   * @param resource the resource instance
+   * @returns array container
+   */
   push(resource: Resource) {
     this.container.push(resource);
     return this.container;
   }
 
+  /**
+   * Merge resources with container
+   * @param arr Array of resources
+   * @returns array container
+   */
   merge(arr: Resource[]) {
     this.container = this.container.concat(arr);
     return this.container;
